a601726dcb815baebb0c4739bc0f33a94f221234
ducmint864 <nddminh2021@gmail.com>
2025-04-07 22:24:37 +0700

    feat: enhance log processing and add indexing for PoolCreated event

--- indexer/src/main.ts
+++ indexer/src/main.ts
@@ -1,11 +1,15 @@
 import "reflect-metadata";
 import { EvmBatchProcessor } from "@subsquid/evm-processor";
-import { User, Project } from "./model/generated";
-import { TypeormDatabase } from "@subsquid/typeorm-store";
+import { User, Project, Pool, PoolType } from "./model/generated";
+import { Store, TypeormDatabase } from "@subsquid/typeorm-store";
+import { BlockData, Log, DataHandlerContext } from "@subsquid/evm-processor";
+import { ethers } from "ethers";
 import * as projectLibraryAbi from "./typegen-abi/ProjectLibrary";
+import * as launchpoolLibraryAbi from "./typegen-abi/LaunchpoolLibrary";
 import "dotenv/config";
 
 const CHAIN_ID = 1287; // Moonbase Alpha testnet
+const BLOCK_TIME = 6; // For Moonbase Alpha
 
 const config = {
 	ethereum: {
@@ -24,6 +28,8 @@ const config = {
 
 type Network = "moonbase_alpha" | "ethereum" | "sepolia";
 const network: Network = "moonbase_alpha";
+
+const provider = new ethers.providers.JsonRpcProvider(config[network].rpc);
 
 // Squid DB for handling indexer's internal stats such as indexer health, etc.
 const squidDb = new TypeormDatabase({
@@ -44,6 +50,11 @@ const processor = new EvmBatchProcessor()
 		address: ["0x2CD45db1754b74dddbE42F742BB10B70D0AC7819".toLowerCase()], // ProjectHubProjeUpgradable Proxy contract
 		topic0: [projectLibraryAbi.events.ProjectCreated.topic],
 		transaction: true,
+	})
+	.addLog({
+		address: ["0x2CD45db1754b74dddbE42F742BB10B70D0AC7819".toLowerCase()], // ProjectHubProjeUpgradable Proxy contract
+		topic0: [launchpoolLibraryAbi.events.LaunchpoolCreated.topic],
+		transaction: true,
 	});
 
 // Debug
@@ -52,50 +63,194 @@ console.log(
 	projectLibraryAbi.events.ProjectCreated.topic
 );
 
-processor.run(squidDb, async (ctx) => {
-	// const projects: Array<Prisma.ProjectCreateManyInput> = [];
-	const projects: Project[] = [];
+type LogsHandler = (
+	ctx: DataHandlerContext<Store>,
+	pendingLogs: Log[],
+	moreProps?: object
+) => Promise<{
+	success: boolean;
+	error: string | null;
+	unprocessedLogs: Log[];
+}>;
 
+const logsDispatch = new Map<
+	string,
+	{ pendingLogs: Log[]; logsHandler: LogsHandler }
+>();
+
+logsDispatch.set(projectLibraryAbi.events.ProjectCreated.topic, {
+	pendingLogs: [],
+	logsHandler: handleProjectCreated,
+});
+logsDispatch.set(launchpoolLibraryAbi.events.LaunchpoolCreated.topic, {
+	pendingLogs: [],
+	logsHandler: handleLaunchpoolCreated,
+});
+
+processor.run(squidDb, async (ctx) => {
+	// Aggregeate logs by topic
 	for (const block of ctx.blocks) {
 		for (const log of block.logs) {
-			const { projectId, projectOwner } =
-				projectLibraryAbi.events.ProjectCreated.decode(log);
-			projects.push(
-				new Project({
-					id: projectId.toString(),
-					chainId: CHAIN_ID,
-					projectOwner: projectOwner.toString(),
-					createdAt: new Date(block.header.timestamp),
-					txHash: log.getTransaction().hash,
-				})
+			const dispatch = logsDispatch.get(log.topics[0]);
+			if (!dispatch) continue;
+
+			console.log("Block timestamp from block is: ", log.block.timestamp);
+			console.log(
+				"Block.timestamp here is miliseconds since 1/1/1970, don't mismatch it with seconds"
 			);
+
+			dispatch.pendingLogs.push(log);
 		}
 	}
 
+	// Process aggregated batches of logs
+	await Promise.all(
+		Array.from(logsDispatch.entries()).map(async ([_, dispatch]) =>
+			dispatch.logsHandler(ctx, dispatch.pendingLogs)
+		)
+	);
+});
+
+async function handleProjectCreated(
+	ctx: DataHandlerContext<Store>,
+	pendingLogs: Log[],
+	moreProps?: object
+): Promise<{
+	success: boolean;
+	error: string | null;
+	unprocessedLogs: Log[];
+}> {
+	const projects: Project[] = [];
+
+	for (const log of pendingLogs) {
+		const { projectId, projectOwner } =
+			projectLibraryAbi.events.ProjectCreated.decode(log);
+		projects.push(
+			new Project({
+				id: projectId.toString(),
+				chainId: CHAIN_ID,
+				projectOwner: projectOwner.toString(),
+				createdAt: new Date(log.block.timestamp),
+				txHash: log.getTransaction().hash,
+			})
+		);
+	}
+
 	// Extract unique project owners
-	const uniqueOwnerIds = new Set<string>(projects.map((p) => p.projectOwner));
+	const uniqueAddresses = new Set<string>(
+		projects.map((p) => p.projectOwner)
+	);
 	const uniqueOwners: User[] = [];
 
-	uniqueOwnerIds.forEach((id) => {
+	for (const address of uniqueAddresses) {
 		const createdAt =
-			projects.find((p) => p.projectOwner === id)?.createdAt ??
+			projects.find((p) => p.projectOwner === address)?.createdAt ??
 			new Date();
-		uniqueOwners.push(
-			new User({
-				id: id,
+		// Check if user already exists in the database
+		let user = await ctx.store.findOneBy(User, {
+			id: address,
+		});
+
+		if (user) {
+			console.log("User already exists: ", user.id);
+			user.lastActive = createdAt;
+		} else {
+			console.log("New user: ", address);
+			user = new User({
+				id: address,
 				firstSeen: createdAt,
 				lastActive: createdAt,
 				totalPoolsParticipated: 0,
 				totalStaked: 0n,
-			})
-		);
-	});
+			});
+		}
+
+		uniqueOwners.push(user);
+	}
+
+	console.log("New projects: ", projects.length);
 
 	// Insert users using ctx.store (will handle duplicates automatically)
 	await ctx.store.save(uniqueOwners);
-
-	console.log("New projects: ", projects.length);
 	await ctx.store.save(projects);
 
-	// TODO: add fallback mechanism: add to queue if prismaClient fails
-});
+	// TODO: implement this later
+	return { success: true, error: null, unprocessedLogs: [] };
+}
+
+async function handleLaunchpoolCreated(
+	ctx: DataHandlerContext<Store>,
+	pendingLogs: Log[],
+	moreProps?: object
+): Promise<{
+	success: boolean;
+	error: string | null;
+	unprocessedLogs: Log[];
+}> {
+	const secondsBetweenBlocks = (from: number, to: number): number => {
+		const signedBlockDelta = from - to;
+		return signedBlockDelta * BLOCK_TIME;
+	};
+
+	const pools: Pool[] = pendingLogs.map((log) => {
+		const launchpoolCreated =
+			launchpoolLibraryAbi.events.LaunchpoolCreated.decode(log);
+
+		return new Pool({
+			id: launchpoolCreated.poolId.toString(),
+			chainId: CHAIN_ID,
+			txHash: log.getTransaction().hash,
+			projectId: launchpoolCreated.projectId.toString(),
+			poolAddress: launchpoolCreated.poolAddress.toString(),
+			isListed: true, // for now (soon refactor)
+			poolType: PoolType.LAUNCHPOOL,
+
+			// Time tracking
+			startBlock: launchpoolCreated.startBlock,
+			endBlock: launchpoolCreated.endBlock,
+			startDate: new Date(
+				Date.now() +
+					secondsBetweenBlocks(
+						log.block.height,
+						Number(launchpoolCreated.startBlock)
+					) *
+						1000
+			),
+			endDate: new Date(
+				Date.now() +
+					secondsBetweenBlocks(
+						log.block.height,
+						Number(launchpoolCreated.endBlock)
+					) *
+						1000
+			),
+
+			// Asset tracking
+			projectTokenAddress: launchpoolCreated.projectToken.toString(),
+			vAssetAddress: launchpoolCreated.vAsset.toString(),
+			nativeAssetAddress: "", // fill later
+
+			// Live stats
+			totalStaked: 0n,
+			totalStakers: 0,
+
+			// APY information
+			stakerAPY: 0,
+			ownerAPY: 0,
+			platformAPY: 0,
+			combinedAPY: 0,
+
+			// Time metadata
+			createdAt: new Date(log.block.timestamp),
+			updatedAt: new Date(log.block.timestamp),
+		});
+	});
+
+	await ctx.store.save(pools);
+
+	return {
+		success: true,
+		error: null,
+		unprocessedLogs: [],
+	};
+}
