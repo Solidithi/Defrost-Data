1f9444a5de1b13d4577f10d776a8b5539807a219
ducmint864 <nddminh2021@gmail.com>
2025-04-20 21:00:26 +0700

    feat: new contract ABIs

--- indexer/src/typegen-abi/LaunchpoolLibrary.ts
+++ indexer/src/typegen-abi/LaunchpoolLibrary.ts
@@ -1,31 +1,13 @@
-import * as p from "@subsquid/evm-codec";
-import { event, fun, viewFun, indexed, ContractBase } from "@subsquid/evm-abi";
-import type {
-	EventParams as EParams,
-	FunctionArguments,
-	FunctionReturn,
-} from "@subsquid/evm-abi";
+import * as p from '@subsquid/evm-codec'
+import { event, fun, viewFun, indexed, ContractBase } from '@subsquid/evm-abi'
+import type { EventParams as EParams, FunctionArguments, FunctionReturn } from '@subsquid/evm-abi'
 
 export const events = {
-	LaunchpoolCreated: event(
-		"0x9574e12b40ffbb140b76a06609bf8507d243b9b7d910fe5a228c4a5bcfc5ebdb",
-		"LaunchpoolCreated(uint64,uint8,uint64,address,address,address,uint128,uint128)",
-		{
-			projectId: indexed(p.uint64),
-			poolType: indexed(p.uint8),
-			poolId: p.uint64,
-			projectToken: p.address,
-			vAsset: indexed(p.address),
-			poolAddress: p.address,
-			startBlock: p.uint128,
-			endBlock: p.uint128,
-		}
-	),
-};
+    LaunchpoolCreated: event("0x6c2fd29867c02c489edbf2f64902041845d1f68921425b999b9f233155870fba", "LaunchpoolCreated(uint64,uint8,uint64,address,address,address,address,uint128,uint128)", {"projectId": indexed(p.uint64), "poolType": indexed(p.uint8), "poolId": p.uint64, "projectToken": p.address, "vAsset": indexed(p.address), "nativeAsset": p.address, "poolAddress": p.address, "startBlock": p.uint128, "endBlock": p.uint128}),
+}
 
-export class Contract extends ContractBase {}
+export class Contract extends ContractBase {
+}
 
 /// Event types
-export type LaunchpoolCreatedEventArgs = EParams<
-	typeof events.LaunchpoolCreated
->;
+export type LaunchpoolCreatedEventArgs = EParams<typeof events.LaunchpoolCreated>
