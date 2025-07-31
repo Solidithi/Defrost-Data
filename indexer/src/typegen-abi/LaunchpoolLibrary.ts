610be07055a4b9d7a0486022655a4bb581fbac01
mailinh <nddminh2021@gmail.com>
2025-07-31 17:23:36 +0700

    chore: update LaunchpoolCreated event signature and parameters in LaunchpoolLibrary.ts

--- indexer/src/typegen-abi/LaunchpoolLibrary.ts
+++ indexer/src/typegen-abi/LaunchpoolLibrary.ts
@@ -3,7 +3,7 @@ import { event, fun, viewFun, indexed, ContractBase } from '@subsquid/evm-abi'
 import type { EventParams as EParams, FunctionArguments, FunctionReturn } from '@subsquid/evm-abi'
 
 export const events = {
-    LaunchpoolCreated: event("0x6c2fd29867c02c489edbf2f64902041845d1f68921425b999b9f233155870fba", "LaunchpoolCreated(uint64,uint8,uint64,address,address,address,address,uint128,uint128)", {"projectId": indexed(p.uint64), "poolType": indexed(p.uint8), "poolId": p.uint64, "projectToken": p.address, "vAsset": indexed(p.address), "nativeAsset": p.address, "poolAddress": p.address, "startBlock": p.uint128, "endBlock": p.uint128}),
+    LaunchpoolCreated: event("0x61dc01716ac96577ed8bed3d032962de7586eb08f6c84fe7f84648d1d0abe177", "LaunchpoolCreated(uint64,uint8,uint64,address,uint256,address,address,address,uint128,uint128)", {"projectId": indexed(p.uint64), "poolType": indexed(p.uint8), "poolId": p.uint64, "projectToken": p.address, "projectTokenAmount": p.uint256, "vAsset": indexed(p.address), "nativeAsset": p.address, "poolAddress": p.address, "startBlock": p.uint128, "endBlock": p.uint128}),
 }
 
 export class Contract extends ContractBase {
