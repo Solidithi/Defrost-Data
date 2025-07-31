544bd87eb092de91245bbcde7ca544113f3216b3
mailinh <nddminh2021@gmail.com>
2025-08-01 03:50:11 +0700

    refactor: un-nest try-catch hell

--- indexer/src/tasks/actions/snapshot-platform-metrics.ts
+++ indexer/src/tasks/actions/snapshot-platform-metrics.ts
@@ -2,23 +2,22 @@ import { ethers } from "ethers";
 import { prismaClient, logger, ethersProvider } from "../../singletons";
 import { selectedChain } from "../../config";
 import { normalizeAddress } from "../../utils";
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 
 export async function snapshotPlatformMetrics(): Promise<void> {
-	// Fetch, process, calculate needed data
 	try {
 		const blockNumber = await ethersProvider.getBlockNumber();
 		const block = await ethersProvider.getBlock(blockNumber);
 		const timestamp = block.timestamp;
 
 		const totalProjects = await prismaClient.project.count();
-		console.log("Total Projects: ", totalProjects);
+		logger.debug("Total Projects: ", totalProjects);
 
 		const totalLaunchpools = await prismaClient.launchpool.count();
-		console.log("Total Launchpools: ", totalLaunchpools);
+		logger.debug("Total Launchpools: ", totalLaunchpools);
 
 		const totalUniqueUsers = await prismaClient.user.count();
-		console.log("Total Unique Users: ", totalUniqueUsers);
+		logger.debug("Total Unique Users: ", totalUniqueUsers);
 
 		// Aggregate total amount of tokens distributed launchpools
 		const resultTokensDistributed = await prismaClient.$queryRaw`
@@ -52,103 +51,96 @@ export async function snapshotPlatformMetrics(): Promise<void> {
 				v_asset_address: true,
 			},
 		});
+
 		const poolUSDAmounts = [];
-		try {
-			for (const pool of pools) {
-				logger.info(
-					`Processing pool with asset address ${pool.v_asset_address}`
+		for (const pool of pools) {
+			logger.info(
+				`Processing pool with asset address ${pool.v_asset_address}`
+			);
+			const totalStaked = pool.total_staked;
+			const { decimals, id } = selectedChain.tokens.find(
+				(token) =>
+					normalizeAddress(token.address) ===
+					normalizeAddress(pool.v_asset_address)
+			);
+
+			if (!decimals) {
+				throw new Error(
+					`Decimals not found for token address: ${pool.v_asset_address}`
 				);
-				const totalStaked = pool.total_staked;
-				const { decimals, id } = selectedChain.tokens.find(
-					(token) =>
-						normalizeAddress(token.address) ===
-						normalizeAddress(pool.v_asset_address)
-				);
+			}
 
-				if (!decimals) {
-					logger.error(
-						`Decimals not found for token address: ${pool.v_asset_address}`
-					);
-					continue;
-				}
+			// Convert totalStaked to a valid BigInt
+			const totalStakedBigInt = BigInt(Number(totalStaked)); // Convert scientific notation to integer string
+			const offchainAmount = ethers.formatUnits(
+				totalStakedBigInt,
+				decimals
+			);
 
-				try {
-					// Convert totalStaked to a valid BigInt
-					const totalStakedBigInt = BigInt(Number(totalStaked)); // Convert scientific notation to integer string
-					const offchainAmount = ethers.formatUnits(
-						totalStakedBigInt,
-						decimals
-					);
+			try {
+				const url = `https://api.coingecko.com/api/v3/coins/${id}`;
+				const options = {
+					method: "GET",
+					headers: {
+						accept: "application/json",
+						"x-cg-demo-api-key": process.env.API_KEY,
+					},
+					timeout: 5000,
+				};
 
-					try {
-						const url = `https://api.coingecko.com/api/v3/coins/${id}`;
-						const options = {
-							method: "GET",
-							headers: {
-								accept: "application/json",
-								"x-cg-demo-api-key": process.env.API_KEY,
-							},
-							timeout: 5000,
-						};
+				const response = await axios.get(url, options);
+				const data = response.data;
+				const priceInUSD = data.market_data.current_price.usd;
 
-						const response = await axios.get(url, options);
-						const data = response.data;
-						const priceInUSD = data.market_data.current_price.usd;
-
-						logger.info(`Price for ${id} in USD: ${priceInUSD}`);
-						await new Promise((resolve) =>
-							setTimeout(resolve, 500)
-						); // Delay for 0.5 second
-						poolUSDAmounts.push(
-							priceInUSD * parseFloat(offchainAmount)
-						);
-
-						// const url = `https://api.coingecko.com/api/v3/simple/price?ids=${id}&vs_currencies=usd`;
-						// 	const response = await axios.get(url, { timeout: 5000 });
-						// 	const data = response.data;
-						// 	logger.info(
-						// 		`Price for ${id} in USD: ${data[id].usd}`
-						// 	);
-						// 	await new Promise(resolve => setTimeout(resolve, 5000)); // Delay for 1 second
-						// 	poolUSDAmounts.push(data[id].usd * parseFloat(offchainAmount));
-						// 	logger.info("Passed")
-						// 	logger.info(
-						// 		data[id].usd * parseFloat(offchainAmount),
-						// 		`Total Staked Amount in USD ${id}: `
-						// 	)
-					} catch (error) {
-						logger.error(
-							`Error fetching price for ${id}: ${error}`
-						);
+				logger.info(`Price for ${id} in USD: ${priceInUSD}`);
+				await new Promise((resolve) => setTimeout(resolve, 500)); // Delay for 0.5 second
+				poolUSDAmounts.push(priceInUSD * parseFloat(offchainAmount));
+			} catch (error) {
+				logger.error(`Error fetching price for ${id}: ${error}`);
+				if (error instanceof AxiosError && error.response.status) {
+					switch (error.response.status) {
+						case 400:
+							throw new Error(`Bad request for coin: ${id}`);
+						case 404:
+							throw new Error(`Coin not found: ${id}`);
+						case 429:
+							logger.warn(
+								`Provider rate limit exceeded for coin: ${id}. Retrying after delay...`
+							);
+							await new Promise((resolve) =>
+								setTimeout(resolve, 1000)
+							); // Retry after 1 second
+							break;
+						default:
+							logger.error(
+								`Axios error: ${error.message} - ${error.response?.status} - ${error.response?.data}`
+							);
 					}
-				} catch (error) {
-					console.error(
-						`Error converting totalStaked for pool ${pool.v_asset_address}: ${error}`
-					);
 				}
 			}
-		} catch (error) {
-			logger.error(`Error fetching pools: ${error}`);
 		}
 
-		console.log("Total Staked Amount Off Chain in Pools: ", poolUSDAmounts);
+		logger.debug(
+			"Total Staked Amount Off Chain in Pools: ",
+			poolUSDAmounts
+		);
 		const totalStakedAmount: number = poolUSDAmounts.reduce(
 			(acc, amount) => acc + amount,
 			0
 		);
-		console.log(
+		logger.debug(
 			"Total Staked Amount Off Chain in Pools: ",
 			totalStakedAmount
 		);
 
 		const totalStakedTx = await prismaClient.launchpool_stake.count();
-		console.log("Total Staked Tx: ", totalStakedTx);
+		logger.debug("Total Staked Tx: ", totalStakedTx);
 
 		const totalUnstakedTx = await prismaClient.launchpool_unstake.count();
-		console.log("Total Unstaked Tx: ", totalUnstakedTx);
+		logger.debug("Total Unstaked Tx: ", totalUnstakedTx);
 
 		const totalTx = totalStakedTx + totalUnstakedTx;
-		console.log("Total Tx: ", totalTx);
+		logger.debug("Total Tx: ", totalTx);
 
 		//Insert data into PlatformMetricsSnapshots table
 		const insertData = await prismaClient.platform_metrics_snapshots.create(
@@ -171,7 +163,7 @@ export async function snapshotPlatformMetrics(): Promise<void> {
 			`Successfully inserted platform metrics snapshot with ID: ${insertData.id}`
 		);
 	} catch (error) {
-		logger.error(`Error fetching platform metrics: ${error}`);
+		logger.error(`Error snapshoting platform metrics: ${error}`);
 		return;
 	}
 }
